6915906cb3c1283c2d820a8e59051f55ac515c46
Daniel Gallardo <dgallardo.c@outlook.com>
2022-09-30 13:47:57 -0500

    Add User information component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Content.css'
 import Sidebar from './Sidebar'
+import UserCV from './UserCV'
 
 class Content extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Content extends React.Component {
     return (
       <div className='Content'>
         <Sidebar profile={user.profile} />
+        <UserCV aim={user.aim} experience={user.experience} education={user.education} />
         {/* User information: picture, name, country, address, contact: [phone, email] */}
         {/* Professional aim: Position title, Salary, Description */}
         {/* Work Experience: [{Company name, position title, tasks:title,description, date of work:start,end}] */}
---
+++ src/components/UserCV.js
@@ -0,0 +1,46 @@
+import React from "react";
+import './UserCV.css'
+
+class UserCV extends React.Component {
+  render() {
+    const { aim, experience, education } = this.props
+    return (
+      <div className="UserCV">
+        <div className="UserCV__card UserCV__aim">
+          <p className="UserCV__card__title">Professional Aim</p>
+          <div className="UserCV__card--position">
+            <p className="UserCV__aim__text--light UserCV__aim__text--offset-up">Title</p>
+            <p className="UserCV__aim__text--title UserCV__aim__text--offset-up">{aim.title}</p>
+            <p className="UserCV__aim__text--p">{aim.description}</p>
+          </div>
+        </div>
+        <div className="UserCV__card UserCV__experience">
+          <p className="UserCV__card__title">Professional experience</p>
+          {experience.map((work, index) => {
+            return (
+              <div className="UserCV__block" key={'work-' + index}>
+                <p className="UserCV__aim__text--title">{work.position} at {work.name}</p>
+                <p className="UserCV__aim__text--light UserCV__aim__text--offset-down">{work.from.start} - {work.from.end}</p>
+                <p className="UserCV__aim__text--p">{work.tasks}</p>
+              </div>
+            )
+          })}
+        </div>
+        <div className="UserCV__card UserCV__education">
+          <p className="UserCV__card__title">Education</p>
+          {education.map((school, index) => {
+            return (
+              <div className="UserCV__block" key={'school-' + index}>
+                <p className="UserCV__aim__text--title">{school.name}</p>
+                <p className="UserCV__aim__text--light UserCV__aim__text--offset-down">{school.from.start} - {school.from.end}</p>
+                <p className="UserCV__aim__text--p">{school.title}</p>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default UserCV
