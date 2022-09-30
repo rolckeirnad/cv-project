dc59be12b5cb180956023c9fd4ed47699a38e43f
Daniel Gallardo <dgallardo.c@outlook.com>
2022-09-30 15:43:35 -0500

    Add state for switch between component and form

--- src/components/UserCV.js
+++ src/components/UserCV.js
@@ -2,18 +2,40 @@ import React from "react";
 import './UserCV.css'
 
 class UserCV extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      edit: {
+        aim: false,
+        experience: false,
+        education: false,
+      }
+    }
+  }
+
+  toggleEdit(e, section) {
+    e.preventDefault()
+    this.setState({
+      edit: {
+        ...this.state.edit,
+        [section]: !this.state.edit[section],
+      }
+    })
+  }
+
   render() {
     const { aim, experience, education } = this.props
+    const { edit } = this.state
     return (
       <div className="UserCV">
         <div className="UserCV__card UserCV__aim">
-          <p className="UserCV__card__title">Professional Aim</p>
-          <div className="UserCV__card--position">
-            <p className="UserCV__aim__text--light UserCV__aim__text--offset-up">Title</p>
-            <p className="UserCV__aim__text--title UserCV__aim__text--offset-up">{aim.title}</p>
-            <p className="UserCV__aim__text--p">{aim.description}</p>
-          </div>
-        </div>
+            <p className="UserCV__card__title">Professional Aim</p>
+            <div className="UserCV__card--position">
+              <p className="UserCV__aim__text--light UserCV__aim__text--offset-up">Title</p>
+              <p className="UserCV__aim__text--title UserCV__aim__text--offset-up">{aim.title}</p>
+              <p className="UserCV__aim__text--p">{aim.description}</p>
+            </div>
+            </div>
         <div className="UserCV__card UserCV__experience">
           <p className="UserCV__card__title">Professional experience</p>
           {experience.map((work, index) => {
