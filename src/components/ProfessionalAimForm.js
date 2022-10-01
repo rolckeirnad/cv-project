4f786efd5d4d2c2358b0f0aee37763f450440c49
Daniel Gallardo <dgallardo.c@outlook.com>
2022-09-30 21:46:42 -0500

    Add feature to edit data in place

--- src/components/ProfessionalAimForm.js
+++ src/components/ProfessionalAimForm.js
@@ -9,6 +9,8 @@ class ProfessionalAimForm extends React.Component {
     }
     this.inputOnChange = this.inputOnChange.bind(this)
     this.textareaOnChange = this.textareaOnChange.bind(this)
+    this.updateInputs = this.updateInputs.bind(this)
+    this.readInputs = this.readInputs.bind(this)
   }
 
   inputOnChange(e) {
@@ -25,22 +27,35 @@ class ProfessionalAimForm extends React.Component {
     })
   }
 
-  getInputs() {
+  readInputs(elements) {
+    let obj = {}
+    for (let each of elements) {
+      if (each.id) obj[each.id] = each.value
+    }
+    return obj
+  }
 
+  updateInputs(e) {
+    e.preventDefault()
+    const form = e.target.parentElement.parentElement
+    const formInputs = this.readInputs(form.elements);
+    const { updateState, toggle } = this.props
+    toggle('aim')
+    updateState('aim', formInputs)
   }
 
   render() {
     const { toggle } = this.props
     const { input, textarea } = this.state
     return (
-      <form className="UserCV__card--static">
+      <form className="UserCV__card--static" onSubmit={this.updateInputs}>
         <p className="UserCV__card__title">Professional Aim</p>
         <p className="UserCV__aim__text--light UserCV__aim__text--offset-up">Title</p>
         <input onChange={this.inputOnChange} className="UserCV__input" id="title" type="text" value={input} />
         <p className="UserCV__aim__text--light UserCV__aim__text--offset-up">Professional aim</p>
         <textarea onChange={this.textareaOnChange} className="UserCV__textarea" name="" id="description" cols="30" rows="10" value={textarea}></textarea>
         <div className="UserCV__block --flex--space-around">
-          <button className="UserCV__button UserCV__button--green" type="button">Save</button>
+          <button className="UserCV__button UserCV__button--green" type="submit" onClick={this.updateInputs}>Save</button>
           <button className="UserCV__button UserCV__button--red" type="button" onClick={(e) => toggle('aim')}>Cancel</button>
         </div>
       </form>
