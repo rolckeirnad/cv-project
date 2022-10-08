8f7a089a7277e0a051f41059c11a2194058651f0
Daniel Gallardo <dgallardo.c@outlook.com>
2022-10-08 00:08:34 -0500

    Add icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className='Footer'>
-        <a className='Footer__Link' href="https://github.com/rolckeirnad/cv-project">GitHub</a>
+        <a className='Footer__Link' href="https://github.com/rolckeirnad/cv-project">GitHub <i className="bi bi-github"></i></a>
       </footer>
     )
   }
