218b31d982a1ab1cb7b5be6ca5b2ef865cdc97de
Daniel Gallardo <dgallardo.c@outlook.com>
2022-10-08 02:22:21 -0500

    Add reset feature

--- src/App.js
+++ src/App.js
@@ -39,10 +39,48 @@ class App extends React.Component {
         ],
       },
     }
+    this.resetState = this.resetState.bind(this)
     this.setNewState = this.setNewState.bind(this)
     this.setProfile = this.setProfile.bind(this)
     this.updateSection = this.updateSection.bind(this)
     this.deleteEntry = this.deleteEntry.bind(this)
+  }
+  componentDidMount() {
+    this.resetState()
+  }
+
+  resetState() {
+    this.setState({
+      user: {
+        profile: {
+          avatar: 'http://placekitten.com/200/200',
+          name: 'Daniel Gallardo Castillo',
+          country: 'México',
+          city: '',
+          address: 'Click to edit',
+          email: 'your@email.com',
+          phone: '52-12345678',
+          links: [],
+        },
+        aim: { title: 'Web Developer', description: 'Here you can write your professional aim. Click here for edit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, vero culpa? Labore facilis omnis illo, eos voluptatibus sunt repellat beatae explicabo fugit repellendus adipisci quasi, impedit distinctio nobis tenetur quis, ipsa soluta quos pariatur delectus? Labore repellendus itaque sed accusamus?' },
+        experience: [
+          {
+            id: 'experience-1', name: 'Company 1', position: 'Your Position', tasks: 'Here you can add a brief description about your daily tasks during your time working at this company', startMonth: 'December', startYear: '2018', endMonth: 'September', endYear: '2021'
+          },
+          {
+            id: 'experience-2', name: 'Company 2', position: 'Your Position', tasks: 'You can edit these entries when you make a click, click on save to keep changes or cancel to discard', startMonth: 'January', startYear: '2018', endMonth: 'December', endYear: '2018'
+          },
+        ],
+        education: [
+          {
+            id: 'education-1', name: 'Name of School 1', level: 'School level', title: 'Title or study name', startYear: '2012', endYear: '2015'
+          },
+          {
+            id: 'education-2', name: 'Name of School 2', level: 'School level', title: 'Title or study name', startYear: '2016', endYear: '2019'
+          },
+        ],
+      },
+    })
   }
 
   setNewState(section, obj) {
@@ -105,10 +143,10 @@ class App extends React.Component {
   }
 
   render() {
-    const { user } = this.state;
+    const { user } = this.state
     return (
       <div className="App" >
-        <Header />
+        <Header reset={this.resetState} />
         <Content
           user={user}
           updateState={this.setNewState}
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,11 @@ import './Header.css'
 
 class Header extends React.Component {
   render() {
+    const { reset } = this.props
     return (
       <nav className='Header'>
         <p className='Header__Title'>CV Application</p>
-        <button className='Header__Button Header__Button--red'>Reset</button>
+        <button className='Header__Button Header__Button--red' onClick={reset}>Reset</button>
       </nav>
     );
   }
